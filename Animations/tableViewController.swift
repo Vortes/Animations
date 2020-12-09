365a4617b5086e496d0f4990470a38a7dfec5d0f
Alan <zeyu.weng@gmail.com>
2020-12-09 17:45:27 -0500

    added segues

---
+++ Animations/tableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  tableViewController.swift
+//  Animations
+//
+//  Created by Alan Weng on 12/9/20.
+//
+
+import UIKit
+
+class tableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+	@IBAction func dismissButton(_ sender: Any) {
+		dismiss(animated: true, completion: nil)
+	}
+	
+
+}
